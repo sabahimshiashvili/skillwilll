3039fc6e1ad5af363602f638d17c7b1caae72b15
sabahimshiashvili <himsiasvilisaba@gmail.com>
2024-06-28 17:33:14 +0400

    first commit

---
+++ Python/level 01/homework/data.py
@@ -0,0 +1,6 @@
+name = "saba"
+surname = "khimshiashvili"
+age = 18
+height = 179
+
+print("my name is", name, ", my surname is", surname, ", i am", age, "years old", "and my height is", height, "cm")
---
+++ Python/level 02/classwork/lv2.py
@@ -0,0 +1,5 @@
+print('My name is Saba')
+print('My surname is Khimshiashvili')
+print('I am 18')
+print('My moms name is Maka')
+print('My favorite food is steak')
