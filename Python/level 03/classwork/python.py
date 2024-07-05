b2b40c780732f5eda7fa5806ec0aef7defac3fd3
sabahimshiashvili <himsiasvilisaba@gmail.com>
2024-07-05 23:21:17 +0400

    first commit

---
+++ Python/level 03/classwork/python.py
@@ -0,0 +1,16 @@
+maka = 46
+koba = 51
+mari = 15
+mamuka = 75
+
+print(maka + 5)
+print( koba + 5)
+print( mari + 5)
+print( mamuka + 5)
+
+#----------------------------------------------
+
+my_age = 18
+mom_age = 46
+print(my_age)
+print(my_age + 28)
