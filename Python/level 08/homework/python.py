35442b918ed507e4e1bb97c0b92c31a8e41d3fc2
sabahimshiashvili <himsiasvilisaba@gmail.com>
2024-07-25 20:58:06 +0400

    first commit

---
+++ Python/level 08/homework/python.py
@@ -0,0 +1,7 @@
+name = 'Saba'
+surname= 'khimshiashvili'
+age = 18
+my_cat = 'mia'
+country = 'Georgia'
+
+print('Hello my name is' ,name, 'and my surname is', surname, 'i am', age, 'years old. ' 'I am from', country, 'i have a cat, her name is', my_cat)
