8c82b6b4a3c02eabefb2dac18b69acc4953d2e99
sabahimshiashvili <himsiasvilisaba@gmail.com>
2024-07-31 21:25:27 +0400

    first commit

--- Python/level 06/classwork/home2.py
+++ Python/level 06/classwork/home2.py
@@ -1,6 +1,6 @@
 number = 25                      # integer 
 text = "bla bla bla"             # string 
-decimal_number = (3.14)          # list 
+decimal_number = (3.14)          # float
 
 print(number)
 print(text)
